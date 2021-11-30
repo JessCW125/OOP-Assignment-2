202925f7936bf775418c8fe37cbfbf0dba259cec
JessCW125 <jessica.wong4@ontariotechu.net>
2021-11-30 02:06:11 -0500

    small edits in formatting

--- OOP Assignment 2 JCW 100783069/src/TestDriver.java
+++ OOP Assignment 2 JCW 100783069/src/TestDriver.java
@@ -20,10 +20,11 @@ public class TestDriver {
           Student sarah = new Student("100523332", "Sarah Lee");
           Student emily = new Student("100512132", "Emily Johnson");
           Student alex = new Student("100232732", "Alex Williams");
-          Student jane = new Student("100783068", "Jane Holmes");
+          Student jane = new Student("100783068", "Jane Fraser");
 
           System.out.println(john);
           System.out.println(sarah);
+
           john.registerFor(engr1200);
           john.registerFor(sofe2800);
           emily.registerFor(engr1200);
@@ -31,6 +32,7 @@ public class TestDriver {
           jane.registerFor(mece2430);
           jane.registerFor(engr1200);
           sarah.registerFor(mece2430);
+
           System.out.println(engr1200);
           System.out.println(sofe2800);
           System.out.println(mece2430);
@@ -56,8 +58,7 @@ public class TestDriver {
             sarah.registerFor(engr1200);
             engr1200.addStudentToCourse(sarah);
             ecse.registerStudentCourseInDepartment(sarah,engr1200);
-            
-            
+
             sarah.registerFor(sofe2800);
             sofe2800.addStudentToCourse(sarah);            
             ecse.registerStudentCourseInDepartment(sarah,sofe2800);
