17e27f31819dc6232ebb2a84a390d2f35fc62e4f
JessCW125 <jessica.wong4@ontariotechu.net>
2021-11-30 23:39:56 -0500

    added javadoc commenting to Student class

--- OOP Assignment 2 JCW 100783069/src/Student.java
+++ OOP Assignment 2 JCW 100783069/src/Student.java
@@ -1,11 +1,22 @@
 import java.util.Vector;
 
+/***
+ * This class is a child class of the Person class. It contains additional attributes from the Person class such as
+ * student ID and a list of courses they are registered in. This class also contains methods that registers the student
+ * in classes and checks if they are in a course.
+ */
+
 public class Student extends Person{
 
   private String id;
   private String name;
   private Vector courses; // contains all courses the student is registered in
 
+	/***
+	 * Constructor for the student.
+	 * @param stdId the 9 digit number that is the student's student ID number
+	 * @param stdName the name of the student
+	 */
 	public Student(String stdId, String stdName) {
 
 		//call parent class constructor
@@ -22,20 +33,31 @@ public class Student extends Person{
 
 	//getters for name and ID
 
-
-  	  public String getName() {
+	/***
+	 * gets the name of the student
+	 * @return a string of the student's name
+	 */
+	public String getName() {
 
 		return name;
 
   	  }
 
-  	  public String getId() {
+	/***
+	 * Returns the student's ID number
+	 * @return the student's ID as a String
+	 */
+	public String getId() {
 
 		return id;
 
   	  }
 
-    public String toString() {
+	/***
+	 * This returns a String that will display all of the Student's info
+	 * @return a String of the student's ID number and name and, on a line after, a list of the courses they are taking
+	 */
+	public String toString() {
      // return a string representation of a student using the following format:
      // 100234546 John McDonald
      // Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
@@ -44,10 +66,11 @@ public class Student extends Person{
 
   	}
 
-	  //This method adds a student to a course and updates the list of courses the student
-	// is registered in, the list of students registered in the course and the list of students
-	// in the department
-  	public void registerFor(Course course){
+	/***This method adds a student to a course and updates the list of courses the student is registered in, the list
+	 * of students registered in the course and the list of students in the department
+	 * @param course the Course the student is registering for
+	 */
+	public void registerFor(Course course){
 
 		//checks if the student is already in the course or not
 		if (!(courses.contains(course))){
@@ -60,7 +83,11 @@ public class Student extends Person{
 
 	}
 
-	//This method checks whether or not a student is in a certain course
+	/***
+	 * This method checks whether or not a student is in a certain course
+	 * @param course the course that we are checking if the student is in or not
+	 * @return True will be returned if the student is taking the course and it is in their course list and vice versa
+	 */
 	public Boolean isRegisteredInCourse(Course course){
 
 		return courses.contains(course);//returns true if the course is found in student's courses vector
